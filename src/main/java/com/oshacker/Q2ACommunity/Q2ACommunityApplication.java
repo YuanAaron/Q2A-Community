29f489e07b05df2aa54e9d25fc96afb37f9a3e84
YuanAaron <1500438364@qq.com>
2019-08-09 18:40:20 +0800

    init project

---
+++ src/main/java/com/oshacker/Q2ACommunity/Q2ACommunityApplication.java
@@ -0,0 +1,13 @@
+package com.oshacker.Q2ACommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q2ACommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Q2ACommunityApplication.class, args);
+	}
+
+}
